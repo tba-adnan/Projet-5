ec169c73e64596113e59996e5a2f883c037f4919
tba-adnan <tebbaa.adnane.dev@gmail.com>
2022-12-23 10:23:02 +0100

    fix.

---
+++ Prototype-Premium/app/Http/Controllers/firebaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use Firebase\JWT\JWT;
+// use Firebase\JWT\Key;
+
+class firebaseController extends Controller
+{
+    public function firebaseMain() {
+       
+    }
+}
